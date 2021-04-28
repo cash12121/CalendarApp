0518356afbbf0a498df57a5625929c21e421f974
owenkra <owenckraus@gmail.com>
2021-04-28 10:31:23 -0500

    Created Month View

--- app/src/main/java/edu/kirkwood/calendar/MainActivity.java
+++ app/src/main/java/edu/kirkwood/calendar/MainActivity.java
@@ -1,23 +1,40 @@
 package edu.kirkwood.calendar;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    //Defining CalendarView and Textview
+    CalendarView calendar;
+    TextView date_view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
-
+        //Getting CalendarView and TextView variables
+        calendar = (CalendarView) findViewById(R.id.calendar);
+        date_view = (TextView) findViewById(R.id.date_view);
+        //Setting date listener for the value of DAYS, MONTH, YEARS
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                //Stores the value of the date in a formatted String
+                //Adding 1 in month because index starts with 0
+                String Date = dayOfMonth + "-" + (month+1) + "-" + year;
+                //Set date in TextView, ie display
+                date_view.setText(Date);
+            }
+        });
     }
 
     @Override
