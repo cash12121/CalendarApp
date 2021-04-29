be789446d8c40c2f282705642f4af922eb553970
AbyssalLight <sampospisil2000@gmail.com>
2021-04-28 21:13:28 -0500

    ToolBar works for both avalable views

--- app/src/main/java/edu/kirkwood/calendar/MainActivity.java
+++ app/src/main/java/edu/kirkwood/calendar/MainActivity.java
@@ -1,18 +1,12 @@
 package edu.kirkwood.calendar;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.widget.CalendarView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ParentActivity {
     //Defining CalendarView and Textview
     CalendarView calendar;
     TextView date_view;
@@ -37,41 +31,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_element, menu);
-        return true;
-    }
-
-    @SuppressLint("NonConstantResourceId")
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.calendar_settings:
-                // User chose the "Settings" item, show the app settings UI
-                return true;
-            case R.id.calendar_view:
-                // User chose the "View" item, show the calendar view select UI
-                return true;
-            case R.id.day_item:
-                // User chose "Day View" under the calendar view submenu
-                openDayActivity();
-                return true;
-
-            default:
-                // If we got here, the user's action was not recognized.
-                // Invoke the superclass to handle it.
-                return super.onOptionsItemSelected(item);
-
-        }
-    }
-
-    // Method to create an Intent to open DayActivity
-    // Should only be called if an event listener wants to open this Activity.
-    private void openDayActivity() {
-        Intent intent = new Intent(this, DayActivity.class);
-        startActivity(intent);
-    }
-
 }
---
+++ app/src/main/java/edu/kirkwood/calendar/ParentActivity.java
@@ -0,0 +1,66 @@
+package edu.kirkwood.calendar;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class ParentActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_element, menu);
+        return true;
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.calendar_settings:
+                // User chose the "Settings" item, show the app settings UI
+                return true;
+            case R.id.calendar_view:
+                // User chose the "View" item, show the calendar view select UI
+                return true;
+            case R.id.day_item:
+                // User chose "Day View" under the calendar view submenu
+                openDayActivity();
+                return true;
+            case R.id.week_item:
+                // User chose "Week View" under the calendar view submenu
+
+                return true;
+            case R.id.month_item:
+                // User chose "Month View" under the calendar view submenu
+                openMonthActivity();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    // Method to create an Intent to open DayActivity
+    // Should only be called if an event listener wants to open this Activity.
+    private void openDayActivity() {
+        Intent intent = new Intent(this, DayActivity.class);
+        startActivity(intent);
+    }
+    private void openMonthActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
