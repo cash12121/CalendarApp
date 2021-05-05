7beaad44fbc6f66a7436aceee65874f62c0bad7d
AbyssalLight <sampospisil2000@gmail.com>
2021-05-05 17:50:30 -0500

    Event select start and end dates

--- app/src/main/java/edu/kirkwood/calendar/EventActivity.java
+++ app/src/main/java/edu/kirkwood/calendar/EventActivity.java
@@ -1,41 +1,50 @@
 package edu.kirkwood.calendar;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 
 public class EventActivity extends ParentActivity {
-
+    //Defining CalendarView and Textview
+    CalendarView calendar_start;
+    CalendarView calendar_end;
+    TextView date_view_start;
+    TextView date_view_end;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
         openParentTools();
-
-        Spinner spinnerY = (Spinner) findViewById(R.id.year_spinner);
-        ArrayAdapter<CharSequence> adapterY = ArrayAdapter.createFromResource(this,
-                R.array.year_array, android.R.layout.simple_spinner_item);
-        adapterY.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerY.setAdapter(adapterY);
-
-        Spinner spinnerM = (Spinner) findViewById(R.id.month_spinner);
-        ArrayAdapter<CharSequence> adapterM = ArrayAdapter.createFromResource(this,
-                R.array.month_array, android.R.layout.simple_spinner_item);
-        adapterM.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerM.setAdapter(adapterM);
-
-        Spinner spinnerD = (Spinner) findViewById(R.id.day_spinner);
-        ArrayAdapter<CharSequence> adapterD = ArrayAdapter.createFromResource(this,
-                R.array.day_array, android.R.layout.simple_spinner_item);
-        adapterD.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerD.setAdapter(adapterD);
-
-        Spinner spinnerH = (Spinner) findViewById(R.id.hour_spinner);
-        ArrayAdapter<CharSequence> adapterH = ArrayAdapter.createFromResource(this,
-                R.array.hour_array, android.R.layout.simple_spinner_item);
-        adapterH.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerH.setAdapter(adapterH);
+        //Getting CalendarView and TextView variables
+        calendar_start = (CalendarView) findViewById(R.id.calendar_start);
+        date_view_start = (TextView) findViewById(R.id.date_view_start);
+        //Setting date listener for the value of DAYS, MONTH, YEARS
+        calendar_start.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                //Stores the value of the date in a formatted String
+                //Adding 1 in month because index starts with 0
+                String Date = dayOfMonth + "-" + (month+1) + "-" + year;
+                //Set date in TextView, ie display
+                date_view_start.setText(Date);
+            }
+        });
+        //Getting CalendarView and TextView variables
+        calendar_end = (CalendarView) findViewById(R.id.calendar_end);
+        date_view_end = (TextView) findViewById(R.id.date_view_end);
+        //Setting date listener for the value of DAYS, MONTH, YEARS
+        calendar_end.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                //Stores the value of the date in a formatted String
+                //Adding 1 in month because index starts with 0
+                String Date = dayOfMonth + "-" + (month+1) + "-" + year;
+                //Set date in TextView, ie display
+                date_view_end.setText(Date);
+            }
+        });
     }
 
     @Override
