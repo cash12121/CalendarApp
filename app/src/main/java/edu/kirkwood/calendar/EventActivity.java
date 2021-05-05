09d1b7f3201e3e4df5ebbd8c7ed5640f6e985498
AbyssalLight <sampospisil2000@gmail.com>
2021-05-04 21:21:51 -0500

    Event beginings

---
+++ app/src/main/java/edu/kirkwood/calendar/EventActivity.java
@@ -0,0 +1,16 @@
+package edu.kirkwood.calendar;
+
+import android.os.Bundle;
+import androidx.appcompat.app.ActionBar;
+
+public class EventActivity extends ParentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event);
+        openParentTools();
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+}
